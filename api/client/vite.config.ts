5e60ccd6a6c6bd5b9a33b4f0c254558651738625
shailendra kumar <shailendraftp000@gmail.com>
2023-11-04 04:17:48 +0530

    deploy with new features

--- api/client/vite.config.ts
+++ api/client/vite.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:8080",
-  //       secure: false,
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ["js-big-decimal"],
